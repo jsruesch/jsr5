a751069754acc60a40090472fa8a417d17c1697e
jsruesch <jsruesch@gmail.com>
2020-02-27 14:57:48 -0600

    push before home

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <section>
             <div class="grid-main-gallery">
                 
+            <!-- Quest -->
                 <div class="mgimg">
                     <a href="#0">
                         <img class="full" src="img/projects/quest/mg-quest-01.png"/>
@@ -16,7 +17,7 @@
                     <div class="b-bottom"></div>
                     <div class="b-left"></div>
                 </div>
-
+            <!-- Newts -->
                 <div class="mgimg">
                     <a href="#0">
                         <img class="full" src="img/projects/newts/mg-newts-01.png"/>
@@ -26,7 +27,7 @@
                     <div class="b-bottom"></div>
                     <div class="b-left"></div>
                 </div>
-
+            <!-- Card Table -->
                 <div class="mgimg">
                     <a href="#0">
                         <img class="full" src="img/projects/the-card-table/mg-cardtable-01.png"/>
@@ -36,7 +37,7 @@
                     <div class="b-bottom"></div>
                     <div class="b-left"></div>
                 </div>
-
+            <!-- Destiny -->
                 <div class="mgimg">
                     <a href="#0">
                         <img class="full" src="img/projects/destiny/mg-destiny-gif.gif"/>
@@ -46,7 +47,7 @@
                     <div class="b-bottom"></div>
                     <div class="b-left"></div>
                 </div>
-
+            <!-- Gear -->
                 <div class="mgimg">
                     <a href="#0">
                         <img class="full" src="img/projects/gear/mg-gear-01.png"/>
@@ -56,17 +57,17 @@
                     <div class="b-bottom"></div>
                     <div class="b-left"></div>
                 </div>
-
+            <!-- Cosmos -->
                 <div class="mgimg">
                     <a href="#0">
-                        <img class="full" src="img/projects/allmight/mg-allmight-01.png"/>
+                        <img class="full" src="img/projects/cosmos/mg-cosmos-01.png"/>
                     </a>
                     <div class="b-top"></div>
                     <div class="b-right"></div>
                     <div class="b-bottom"></div>
                     <div class="b-left"></div>
                 </div>
-
+            <!-- All Might -->
                 <div class="mgimg">
                     <a href="#0">
                         <img class="full" src="img/projects/allmight/mg-allmight-01.png"/>
