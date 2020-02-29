d68268cf2b8a9257a2e6075ef08815def42ec189
jsruesch <jake@gofarwell.com>
2020-02-29 10:34:24 -0600

    finish destiny

--- project-destiny.php
+++ project-destiny.php
@@ -20,6 +20,19 @@
                 <img class="full" src="img/projects/destiny/destiny-logo-lineup-01.png" alt="Destiny : CLB Logo Lineup">
             </div>
 
+            <div class="full copy-container pad60  grid-vertical-align-content">
+                <div class="full">
+                    <div class="full">
+                        <h3 class="text-gold">Destiny 2</h3>
+                    </div>
+                    <div class="full">
+                        <p class="text-grey mar15-top">
+                        Over the last few years I've grown to love Destiny 2 and it's community. Ive made a bunch of new friends and have loved learning and growing as a player in the deep, immersive world Destiny 2 provides. When Destiny was chosen as the theme for our Core Skills Clinic Hammersnow Cup game, I was giddy at the chance to make these logos. Inside jokes included, of course! Each of the characters featured here are modeled after actual people in my Destiny clan wearing their favorite gear sets!
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         <!-- Titans -->
             <div class="full mar5-bot">
                 <img class="full" src="img/projects/destiny/destiny-titan-01.png" alt="Destiny : Titans Logo">
@@ -30,28 +43,59 @@
                 <div class="full">
                     <img class="full" src="img/projects/destiny/destiny-titan-03.png" alt="Destiny : Titans Closeup 01"></div>
             </div><!-- END: Titans -->
+            <div class="full copy-container pad60  grid-vertical-align-content">
+                <div class="full">
+                    <div class="full">
+                        <h3 class="text-gold">Crayon Eaters</h3>
+                    </div>
+                    <div class="full">
+                        <p class="text-grey mar15-top">
+                        Titans were the bane of my existence in pvp for the longest time. O.E.M., shoulder charge, striker, the list goes on. It was awful. Naturally I developed a distaste for the class. It gave me great joy adding a crayon snack pack to this illustration. The salt from Titan mains about an Arc subclass wearing exotic armor supposed to be used with the Void subclass was absolutely delicious as well (evil grin).
+                        </p>
+                    </div>
+                </div>
+            </div>
 
         <!-- Warlocks -->
             <div class="full mar5-bot">
-                <img class="full" src="img/projects/destiny/destiny-warlocks-01.png" alt="Destiny : Titans Logo">
+                <img class="full" src="img/projects/destiny/destiny-warlocks-01.png" alt="Destiny : Warlocks Logo">
             </div>
-            <div class="full grid-2col ggap-5">
+            <div class="full grid-2col ggap-5 mar5-bot">
                 <div class="full">
-                    <img class="full" src="img/projects/destiny/destiny-warlocks-02.png" alt="Destiny : Titans Closeup 01"></div>
+                    <img class="full" src="img/projects/destiny/destiny-warlocks-02.png" alt="Destiny : Warlocks Closeup 01"></div>
                 <div class="full">
-                    <img class="full" src="img/projects/destiny/destiny-warlocks-03.png" alt="Destiny : Titans Closeup 01"></div>
+                    <img class="full" src="img/projects/destiny/destiny-warlocks-03.png" alt="Destiny : Warlocks Closeup 02"></div>
             </div><!-- END: Warlocks -->
 
         <!-- Hunters -->
             <div class="full mar5-bot">
-                <img class="full" src="img/projects/destiny/destiny-hunters-01.png" alt="Destiny : Titans Logo">
+                <img class="full" src="img/projects/destiny/destiny-hunters-01.png" alt="Destiny : Hunters Logo">
             </div>
+            <div class="full grid-2col ggap-5 mar5-bot">
+                <div class="full">
+                    <img class="full" src="img/projects/destiny/destiny-hunters-02.png" alt="Destiny : Hunters Closeup 01"></div>
+                <div class="full">
+                    <img class="full" src="img/projects/destiny/destiny-hunters-03.png" alt="Destiny : Hunters Closeup 02"></div>
+            </div><!-- END: Hunters -->
+
             <div class="full grid-2col ggap-5">
                 <div class="full">
-                    <img class="full" src="img/projects/destiny/destiny-hunters-02.png" alt="Destiny : Titans Closeup 01"></div>
+                    <img class="full" src="img/projects/destiny/destiny-platinum-corndog.png" alt="Destiny : Platinum Cordog"></div>
+                    <div class="full copy-container pad60  grid-vertical-align-content">
                 <div class="full">
-                    <img class="full" src="img/projects/destiny/destiny-hunters-03.png" alt="Destiny : Titans Closeup 01"></div>
+                    <div class="full">
+                        <h3 class="text-gold">Platinum Corndog!</h3>
+                    </div>
+                    <div class="full">
+                        <p class="text-grey mar15-top">
+                        My good friend Bjorn is a die hard Platinum Corndog (inside joke - The activity is actually called Iron Banner ) supporter. I had to make sure the sigil for P.C. was on his gear in this illustration!
+                        </p>
+                    </div>
+                </div>
+            </div>
             </div><!-- END: Hunters -->
+
+            
         </section>
     </div>
 </div>
