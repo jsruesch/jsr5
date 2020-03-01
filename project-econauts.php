03e3ab4e14138c598a38fb9e39750efd108eba96
jsruesch <jake@gofarwell.com>
2020-03-01 15:56:54 -0600

    actually add newts

---
+++ project-econauts.php
@@ -0,0 +1,51 @@
+<?php include('./inc/header.php') ?>
+
+<div class="wrapper">
+    <div class="wrapper-content">
+
+    <!-- Project Header -->
+        <section class="project-header">
+            <div class="grid-2col mar10-bot">
+                <div class="full">
+                    <h1 class="header-title">Econauts</h1>
+                </div>
+                <div class="full">
+                    <h4 class="header-catagories">Game UI</h4>
+                </div>
+            </div>
+        </section> <!-- END: Project Header -->
+
+        <section class="project-content">
+            <div class="full mar5-bot">
+                <img class="full" src="img/projects/econauts/econauts-hero-01.png" alt="Newts Voyage Hero">
+            </div>
+
+            <div class="full copy-container pad60  grid-vertical-align-content">
+                <div class="full">
+                    <div class="full">
+                        <h3 class="text-gold">Teaching Environmental Science</h3>
+                    </div>
+                    <div class="full">
+                        <p class="text-grey mar15-top">
+                        Econauts uses real-time-strategy gameplay to teach users about the impact of various types of real-world roles. Loggers, builders, and miners compete against each other to try and make the most profit while at the same time preserving the most environment. This was a real treat to work on. The concept artists and modelers really did a great job with the in-game and character art.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="full grid-2col ggap-5 mar10-bot">
+                
+               <img class="full" src="img/projects/econauts/econauts_01.png" alt="Econauts UI Screenshot">
+               <img class="full" src="img/projects/econauts/econauts_02.png" alt="Econauts UI Screenshot">
+               <img class="full" src="img/projects/econauts/econauts_03.png" alt="Econauts UI Screenshot">
+               <img class="full" src="img/projects/econauts/econauts_04.png" alt="Econauts UI Screenshot">
+               <img class="full" src="img/projects/econauts/econauts_05.png" alt="Econauts UI Screenshot">
+               <img class="full" src="img/projects/econauts/econauts_06.png" alt="Econauts UI Screenshot">
+            </div>
+            
+        </section>
+    </div>
+</div>
+
+<?php include('./inc/footer.php') ?> 
+
---
+++ project-newtsvoyage.php
@@ -0,0 +1,81 @@
+<?php include('./inc/header.php') ?>
+
+<div class="wrapper">
+    <div class="wrapper-content">
+
+    <!-- Project Header -->
+        <section class="project-header">
+            <div class="grid-2col mar10-bot">
+                <div class="full">
+                    <h1 class="header-title">Newts Voyage</h1>
+                </div>
+                <div class="full">
+                    <h4 class="header-catagories">Art Direction, Game UI, Graphic Design, Video</h4>
+                </div>
+            </div>
+        </section> <!-- END: Project Header -->
+
+        <section class="project-content">
+            <div class="full mar5-bot">
+                <img class="full" src="img/projects/newts/newts-hero-01.png" alt="Newts Voyage Hero">
+            </div>
+
+            <div class="full copy-container pad60  grid-vertical-align-content">
+                <div class="full">
+                    <div class="full">
+                        <h3 class="text-gold">Trinitas</h3>
+                    </div>
+                    <div class="full">
+                        <p class="text-grey mar15-top">
+                        I wore many hats during the development of this project. As Art Director I directed a very small team with limited assets and resources in turning this project into a reality. I created the UI, launch trailer, many of the levels, and the promotional meterials for this project. The timeline for development was extremely short so working efficiently deciding what the most effective use of our time was paramount.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="full mar10-bot">                
+                <iframe width="100%" height="500" src="https://www.youtube.com/embed/8NmS-ykHxDs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            <div class="full grid-2col ggap5 mar5-bot">
+                <img class="full" src="img/projects/newts/nv-ss-16x9-07.jpg" alt="Newts Screenshot 01">
+                <div class="full copy-container pad60  grid-vertical-align-content">
+                    <div class="full">
+                        <div class="full">
+                            <h3 class="text-gold">Easy to Learn, Hard to Master</h3>
+                        </div>
+                        <div class="full">
+                            <p class="text-grey mar15-top">
+                            It was really important to me to craft an experience that appealed to a lot of different player types. Im a huge fan of games that have a really high skill ceiling so using different level and ship types I was able to help create an experience that checked that box for me while at the same time appealing to a player who has less gaming experience and prefers a more chill play experience.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="full grid-2col ggap-5 mar5-bot">
+                <img class="full" src="img/projects/newts/nv-ss-16x9-01.jpg" alt="Newts Screenshot 01">
+                <img class="full" src="img/projects/newts/nv-ss-16x9-04.jpg" alt="Newts Screenshot 01">
+                <img class="full" src="img/projects/newts/trinitas.png" alt="Newts Screenshot 01">
+                <img class="full" src="img/projects/newts/nv-ss-16x9-02.jpg" alt="Newts Screenshot 01">
+            </div>
+
+            <div class="full copy-container pad60  grid-vertical-align-content">
+                <div class="full">
+                    <div class="full">
+                        <h3 class="text-gold">Soundtrack</h3>
+                    </div>
+                    <div class="full">
+                        <p class="text-grey mar15-top">
+                        The soundtrack for Newt's Voyage was provided by the incredibly talented electronic music artist <a href="https://hubrid54.bandcamp.com/music" target="_blank">Hubrid</a>. Head over to <a href="https://hubrid54.bandcamp.com/music" target="_blank">their bandcamp page</a> and listen to more of their awesome discography.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    </div>
+</div>
+
+<?php include('./inc/footer.php') ?> 
+
