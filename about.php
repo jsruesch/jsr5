e20be288d465090b5def053f2608b0f355019fe9
jsruesch <jake@gofarwell.com>
2020-03-02 18:43:17 -0600

    Start about page

--- about.php
+++ about.php
@@ -6,14 +6,29 @@
 <div class="wrapper">
     <div class="wrapper-content">
 
-    <!-- Project Header -->
-        <section class="project-header">
-            <div class="full mar10-bot">
-                <h1 class="header-title">About</h1>
-            </div>
-        </section> <!-- END: Project Header -->
 
         <section class="project-content">
+
+        <div class="full">
+            
+            <div class="grid-2col ggap20">
+
+                <div class="portrait-container">
+                    <img class="full mar5-bot" src="img/assets/portrait.png" alt="Jake Ruesch Portrait">
+                    <img class="full mar5-bot" src="img/assets/yaycold.png" alt="Jake Ruesch Portrait">
+                    <img class="full mar5-bot" src="img/assets/bjj.png" alt="Jake Ruesch Portrait">
+                </div>
+                
+                <div class="h-flex copy-container pad60 grid-vertical-align-content">
+                    <div class="full mar20-bot">
+                        <p class="text-grey mar15-top">
+                        I'm Jake. I feel there are few things that work as an outlet for creative expression as well as art. Over the I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I’m into it. In the world of art and design, there is so much to explore it’s a waste not to try out different styles and mediums.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
         </section>
     </div>
