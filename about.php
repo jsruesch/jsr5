426740dd4b67c0731e9a332e341e3209891f3868
jsruesch <jake@gofarwell.com>
2020-03-02 19:04:31 -0600

    ok now Im actually done for the night

--- about.php
+++ about.php
@@ -9,26 +9,38 @@
 
         <section class="project-content">
 
-        <div class="full">
-            
-            <div class="grid-vertical-align-content">
-
-                <div class="portrait-container">
-                    <img class="full mar5-bot" src="img/assets/portrait.png" alt="Jake Ruesch Portrait">
-                    <img class="full mar5-bot" src="img/assets/yaycold.png" alt="Jake Ruesch Portrait">
-                    <img class="full mar5-bot" src="img/assets/bjj.png" alt="Jake Ruesch Portrait">
-                </div>
+            <div class="full">
                 
-                <div class="copy-container pad60 v-flex h-flex ">
-                    <div class="full mar20-bot">
-                        <p class="text-grey mar15-top">
-                        I'm Jake. I feel there are few things that work as an outlet for creative expression as well as art. Over the I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I’m into it. In the world of art and design, there is so much to explore it’s a waste not to try out different styles and mediums.
-                        </p>
-                    </div>
-                </div>
+                <div class="grid-vertical-align-content">
 
+                    <div class="portrait-container">
+                        <img class="full mar5-bot" src="img/assets/portrait.png" alt="Jake Ruesch Portrait">
+                        <img class="full mar5-bot" src="img/assets/yaycold.png" alt="Jake Ruesch Portrait">
+                        <img class="full mar5-bot" src="img/assets/bjj.png" alt="Jake Ruesch Portrait">
+                    </div>
+                    
+                    <div class="copy-container pad60 v-flex h-flex ">
+                        <div class="full mar20-bot">
+                            <p class="text-grey mar15-top">
+                            I'm Jake. I feel there are few things that work as an outlet for creative expression as well as art. Over the I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I’m into it. In the world of art and design, there is so much to explore it’s a waste not to try out different styles and mediums.
+                            </p>
+                        </div>
+                        <div class="full">
+                            <h5 class="text-gold">Skillsets</h5>
+                            <p class="text-grey">
+                            Art Direction, Graphic Design, Photo Retouching, Web Design (HTML, SCSS, Js, Custom Wordpress Theming), Game UI Design, UX Design, Video Editing and Effects
+                            </p>
+                        </div>
+                        <div class="full mar20-top">
+                            <h5 class="text-gold">Software</h5>
+                            <p class="text-grey">
+                            Photoshop, Illustrator, Xd, InDesign, After Effects, Premiere, Unity, Blender, Aseprite
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
 
         </section>
     </div>
