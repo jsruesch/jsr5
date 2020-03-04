a2823e194bdf881c4beee13435dcc557d8ccabe9
jsruesch <jake@gofarwell.com>
2020-03-04 11:00:56 -0600

    finish about page

--- about.php
+++ about.php
@@ -9,20 +9,23 @@
 
         <section class="project-content">
 
-            <div class="full">
-                
-                <div class="grid-vertical-align-content">
+            <div class="full grid-about">
 
-                    <div class="portrait-container">
-                        <img class="full mar5-bot" src="img/assets/portrait.png" alt="Jake Ruesch Portrait">
-                        <img class="full mar5-bot" src="img/assets/yaycold.png" alt="Jake Ruesch Portrait">
-                        <img class="full mar5-bot" src="img/assets/bjj.png" alt="Jake Ruesch Portrait">
-                    </div>
-                    
-                    <div class="copy-container pad60 v-flex h-flex ">
+                <div class="about-portrait-container">
+                    <img class="about-portrait" src="img/assets/portrait.png" alt="Jake Ruesch Portrait">
+                    <img class="about-portrait" src="img/assets/yaycold.png" alt="Jake Ruesch Portrait">
+                    <img class="about-portrait" src="img/assets/bjj.png" alt="Jake Ruesch Portrait">
+                </div>
+
+                
+                <div class=" grid-vertical-align-content">
+                    <div class="copy-container pad60">
+                        <div class="full">
+                            <h3 class="text-gold">Hey! I'm Jake</h3>
+                        </div>
                         <div class="full mar20-bot">
                             <p class="text-grey mar15-top">
-                            I'm Jake. I feel there are few things that work as an outlet for creative expression as well as art. Over the I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I’m into it. In the world of art and design, there is so much to explore it’s a waste not to try out different styles and mediums.
+                             I feel there are few things that work as an outlet for creative expression as well as art. Over the I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I’m into it. In the world of art and design, there is so much to explore it’s a waste not to try out different styles and mediums.
                             </p>
                         </div>
                         <div class="full">
@@ -38,8 +41,8 @@
                             </p>
                         </div>
                     </div>
-
                 </div>
+
             </div>
 
         </section>
