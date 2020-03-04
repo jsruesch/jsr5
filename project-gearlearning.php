5352e981466fd0ef8967c0cfeadb0c72d6503df8
jsruesch <jake@gofarwell.com>
2020-03-04 09:15:53 -0600

    continue gear page work

--- project-gearlearning.php
+++ project-gearlearning.php
@@ -10,7 +10,7 @@
                     <h1 class="header-title">Gear Learning</h1>
                 </div>
                 <div class="full">
-                    <h4 class="header-catagories">Art Direction, Graphic Design, Web Design</h4>
+                    <h4 class="header-catagories">Art Direction, Graphic Design, Web Design, Video</h4>
                 </div>
             </div>
         </section> <!-- END: Project Header -->
@@ -23,11 +23,11 @@
             <div class="full copy-container pad60  grid-vertical-align-content">
                 <div class="full">
                     <div class="full">
-                        <h3 class="text-gold">The Symbol of Peace!</h3>
+                        <h3 class="text-gold">The Gear Learning Brand</h3>
                     </div>
                     <div class="full">
                         <p class="text-grey mar15-top">
-                        Trying to take my digital painting skills beyond plus ultra by painting my favorite character in the history of characters! 
+                        I developed the Gear Learning brand from top to bottom. I developed logos, documentation, business cards, document templates, styleguides, signage, brand assets, swag and merchandise, social media assets, convention booth banners/stands, and much more. 
                         </p>
                     </div>
                 </div>
@@ -38,15 +38,30 @@
             </div>
             
             <div class="full grid-2col ggap5 mar5-bot">
-                
-                <img class="full" src="img/projects/gear/gear-web.gif" alt="All Might Process Sequence">
+                <img class="full" src="img/projects/gear/gear-web.gif" alt="Gear Learning Responsive Web Sequence">
+                <div class="full copy-container pad60  grid-vertical-align-content">
+                    <div class="full">
+                        <div class="full">
+                            <h3 class="text-gold">Video </h3>
+                        </div>
+                        <div class="full">
+                            <p class="text-grey mar15-top">
+                            I also designed and built the website for Gear. The site highlights a lot of the projects I had the privilege of working on with Gear as well! <a href="inc/archive/gearlearning/" target="_blank">View the website here!</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="full grid-3col ggap5 mar5-bot">
-                <img class="full" src="img/projects/gear/gear-tough-mudder.png" alt="All Might Closeup Image">
-                <img class="full" src="img/projects/gear/gear-image-01.png" alt="All Might Closeup Image">
-                <!-- <img class="full" src="img/projects/gear/gear-intro.gif" alt="All Might Process Sequence"> -->
-                <!-- <img class="full" src="img/projects/gear/gear-intro.gif" alt="All Might Process Sequence"> -->
+                <img class="full" src="img/projects/gear/gear-tough-mudder.png" alt="Gear Learning Tough Mudder Image">
+                <img class="full" src="img/projects/gear/gear-image-01.png" alt="Gear Learning Office Wall Image">
+                <img class="full" src="img/projects/gear/gear-booth-02.png" alt="Gear Learning SXSW Edu Booth">
+            </div>
+
+            <div class="full grid-2col ggap5 mar5-bot">
+
+                <img class="full" src="img/projects/gear/gear-intro.gif" alt="Gear Learning Intro Sequence">
             </div>
 
 
