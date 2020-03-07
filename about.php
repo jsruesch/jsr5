b9843accc89827214a005a44a4d709902e17ef8c
jsruesch <jake@gofarwell.com>
2020-03-06 20:09:56 -0600

    typos on about

--- about.php
+++ about.php
@@ -25,7 +25,7 @@
                         </div>
                         <div class="full mar20-bot">
                             <p class="text-grey mar15-top">
-                             I feel there are few things that work as an outlet for creative expression as well as art. Over the I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I’m into it. In the world of art and design, there is so much to explore it’s a waste not to try out different styles and mediums.
+                             I feel there are few things that work as an outlet for creative expression as well as art. Over the years I've developed a deep love for a wide variety of visual designs and mediums. I could be directing a team of artists to execute artistic vision, grabbing a set of images and turning them into a cohesive, dynamic composition, creating a responsive, user-friendly, intuitive website or creating high-detail vector art. You name it; I'm into it. In the world of art and design, there is so much to explore it's a waste not to try out different styles and mediums.
                             </p>
                         </div>
                         <div class="full">
